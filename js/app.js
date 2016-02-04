a47f58eb9c7caa1dee14d84a697a99f991e98163
razawi <raz.kronenberg@gmail.com>
2016-02-04 17:33:13 +0200

    basic routing

--- js/app.js
+++ js/app.js
@@ -1,11 +1,24 @@
 angular.module('teacherApp', ['ngResource', 'ngRoute'])
     .config(['$routeProvider', function($routeProvider) {
-
         $routeProvider
-            .when('/teacher', {
+            .when('/', {
                 controller: 'movieTableManage',
-                controllerAs: 'main',
-                templateUrl: '/public/apps/SaySo-teacher/templates/Partials/movieList.ejs'
+                templateUrl: 'http://127.0.0.1:8888/public/apps/SaySo-teacher/templates/Partials/movieList.html'
             })
             .otherwise('/');
     }]);
+
+/*
+ angular.module('sayso', ['ngRoute', 'ngSanitize'])
+ .constant('MOVIES_URL', 'http://127.0.0.1:8888/rdata/movies')
+ .constant('SUBTITLES_URL', 'http://127.0.0.1:8888/rdata/subtitle')
+ .config(['$routeProvider', function($routeProvider) {
+ $routeProvider
+ .when('/', {
+ controller: 'mainController',
+ controllerAs: 'main',
+ templateUrl: 'http://127.0.0.1:8888/public/apps/SaySo-client/freelancer-mobile/partials/main.html'
+ })
+ .otherwise('/');
+ }]);
+*/
--- js/controllers/teacerInt.js
+++ js/controllers/teacerInt.js
@@ -1,35 +1,10 @@
-var myApp = angular.module('teacherApp', ['ngResource', 'ngRoute']);
+angular.module('teacherApp')
+    .controller('movieTableManage', ['$scope', '$http', function($scope, $http) {
 
-myApp.controller('movieTableManage', ['$scope', '$http', function($scope, $http) {
-
-    $http.get('/dbapi/movies').success(function(response){
+    $http.get('http://127.0.0.1:8888/dbapi/movies').success(function(response){
         console.log('$scope.list at getMovies ');
         $scope.movielist = response;
     })
 
     console.log("Hello World from tableManage");
 }]);
-
-
-/*
- angular.module('sayso', ['ngRoute', 'ngSanitize'])
- .constant('MOVIES_URL', 'data/movies.json')
- .constant('SUBTITLES_URL', 'data/subtitles/movie.orig.json')
- .config(['$routeProvider', function($routeProvider) {
- $routeProvider
- .when('/', {
- controller: 'mainController',
- controllerAs: 'main',
- templateUrl: 'partials/main.html'
- })
- .otherwise('/');
- }]);
-*/
-
-
-
-//$scope, $http) {
-
-//$http.get('/cliplist')
-
-//exports.appctrl = appctrl;
